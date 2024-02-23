f00d6b9bb92e58ef131e5e06804ee27e367cda08
Mohamdy <mohamedhamdy994@gmail.com>
2024-02-23 23:11:40 +0200

    commit

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,7 +1,9 @@
+# Create a file in /tmp
+
 file { 'school':
+  content => 'I love Puppet',
+  group   => 'www-data',
   mode    => '0744',
   owner   => 'www-data',
-  group   => 'www-data',
-  content => 'I love Puppet',
   path    => '/tmp/school',
 }
