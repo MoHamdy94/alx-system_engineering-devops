104bf50cfac534571af63915bd7c5d8a2534ae12
Mohamdy <mohamedhamdy994@gmail.com>
2024-03-26 03:10:51 +0200

    comit

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,34 +1,29 @@
 #!/usr/bin/python3
-"""
-Returns to-do list information for a given employee ID.
-
-This script takes an employee ID as a command-line argument and fetches
-the corresponding user information and to-do list from the JSONPlaceholder API.
-It then prints the tasks completed by the employee.
-"""
-
-import requests
-import sys
-
-
+'''Python script that
+returns information about his todo list progress.
+'''
 if __name__ == "__main__":
-    # Base URL for the JSONPlaceholder API
-    url = "https://jsonplaceholder.typicode.com/"
+    import requests
+    from sys import argv
 
-    # Get the employee information using the provided employee ID
-    employee_id = sys.argv[1]
-    user = requests.get(url + "users/{}".format(employee_id)).json()
+    urlUser = 'https://jsonplaceholder.typicode.com/users/' + argv[1]
+    response_name = requests.get(urlUser)
+    name = response_name.json()['name']
 
-    # Get the to-do list for the employee using the provided employee ID
-    params = {"userId": employee_id}
-    todos = requests.get(url + "todos", params).json()
+    urlTodo = urlUser + '/todos'
+    response_todos = requests.get(urlTodo)
+    tasks = response_todos.json()
+    total_task = len(tasks)
 
-    # Filter completed tasks and count them
-    completed = [t.get("title") for t in todos if t.get("completed") is True]
+    count = 0
+    for task in tasks:
+        if task['completed'] is True:
+            count += 1
 
-    # Print the employee's name and the number of completed tasks
-    print("Employee {} is done with tasks({}/{}):".format(
-        user.get("name"), len(completed), len(todos)))
+    print("Employee {} is done with tasks({}/{}):".format(name, count,
+          total_task))
 
-    # Print the completed tasks one by one with indentation
-    [print("\t {}".format(complete)) for complete in completed]
+    for task in tasks:
+        if task['completed'] is True:
+            task_name = task['title']
+            print("\t {}".format(task_name))
