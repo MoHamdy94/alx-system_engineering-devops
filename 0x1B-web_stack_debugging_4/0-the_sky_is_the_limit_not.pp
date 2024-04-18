6914867bc7d9ad0ddb00a987d3adaa0e08aaa84c
Mohamdy <mohamedhamdy994@gmail.com>
2024-04-18 11:59:22 +0200

    comit

--- 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
+++ 0x1B-web_stack_debugging_4/0-the_sky_is_the_limit_not.pp
@@ -1,14 +1,9 @@
+# Increases the number of requests that nginx can handle.
+# by increasing maximum open files limit for the nginx user.
 
-# Increases the amount of traffic an Nginx server can handle.
+exec { 'set_ulimit_to 5000':
+  command => '/bin/sed -i "s/ULIMIT.*/ULIMIT=\"-n 5000\"/" /etc/default/nginx'
 
-# Increase the ULIMIT of the default file
-exec { 'fix--for-nginx':
-  command => 'sed -i "s/15/4096/" /etc/default/nginx',
-  path    => '/usr/local/bin/:/bin/'
-} ->
-
-# Restart Nginx
-exec { 'nginx-restart':
-  command => 'nginx restart',
-  path    => '/etc/init.d/'
+} -> exec { 'restart nginx':
+  command => '/usr/sbin/service nginx restart',
 }
